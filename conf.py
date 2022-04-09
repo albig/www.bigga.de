82ac3ffc98875f1b50f32d1c713a982532d53f32
Alexander Bigga <alexander@bigga.de>
2022-04-10 00:10:04 +0200

    We do not have a feed yet

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ html_logo = "_static/logo.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_extra_path = ["feed.xml"]
+# html_extra_path = ["feed.xml"]
 html_sidebars = {
     "index": ["hello.html", "sidebar-nav-bs.html"],
     "about": ["hello.html", "sidebar-nav-bs.html"],
